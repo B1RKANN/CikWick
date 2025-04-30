2feff92ef1929a2301ba8fe34ac7c07be0566e15
B1RKANN <birkannbozz@gmail.com>
2025-04-30 19:43:57 +0300

    Paketler eklendi

---
+++ Assets/Scripts/TestingScript.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+
+public class NewMonoBehaviourScript : MonoBehaviour
+{
+    void Awake() {
+        Debug.Log("Hello, World!");
+    }
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    void FixedUpdate()
+    {
+        
+    }
+
+    void LateUpdate()
+    {
+        
+    }
+
+    void OnEnable()//script çalıştığında çalışır
+    {
+        
+    }
+
+    void OnDisable()//script devre dışı kaldığında çalışır
+    {
+        
+    }
+}
