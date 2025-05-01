c5da8aa41a4f9a9e765fc13f844818e4c6363143
Birkan Boz <birkannbozz@gmail.com>
2025-05-01 22:48:28 +0300

    Collectible eklendi

--- Assets/_GameAssets/Scripts/GamePlay/Player/PlayerController.cs
+++ Assets/_GameAssets/Scripts/GamePlay/Player/PlayerController.cs
@@ -12,6 +12,8 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private float _movementSpeed;
     private Rigidbody _playerRigidbody;
 
+    private float _startingMovementSpeed,_startingJumpForce;
+
     [Header("Jump Settings")]
     [SerializeField] private KeyCode _jumpKey;
     [SerializeField] private float _jumpForce;
@@ -42,6 +44,8 @@ public class PlayerController : MonoBehaviour
         _stateController = GetComponent<StateController>();
         _playerRigidbody = GetComponent<Rigidbody>();
         _playerRigidbody.freezeRotation = true;
+        _startingMovementSpeed = _movementSpeed;
+        _startingJumpForce = _jumpForce;
     }
 
     private void Update()
@@ -124,6 +128,7 @@ public class PlayerController : MonoBehaviour
         _canJump = true;
     }
 
+    #region Helper Functions
     private bool IsGrounded(){
         return Physics.Raycast(transform.position, Vector3.down,_playerHighth*0.5f+0.2f, _groundLayer);
     }
@@ -134,4 +139,22 @@ public class PlayerController : MonoBehaviour
     private bool IsSliding(){
         return _isSliding;
     }
+    public void SetMovementSpeed(float speed,float duration){
+        _movementSpeed += speed;
+        Invoke(nameof(ResetMovementSpeed),duration);
+    }
+
+    private void ResetMovementSpeed(){
+        _movementSpeed = _startingMovementSpeed;
+    }
+
+    public void SetJumpForce(float force,float duration){
+        _jumpForce += force;
+        Invoke(nameof(ResetJumpForce),duration);
+    }
+
+    private void ResetJumpForce(){
+        _jumpForce = _startingJumpForce;
+    }
+    #endregion
 }
